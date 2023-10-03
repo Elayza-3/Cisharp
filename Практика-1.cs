422b269572b638155157d826f298143ceb0febd7
Elayza-3 <isip_e.d.grachkova@mpt.ru>
2023-10-03 10:50:39 +0300

    Практическая №2

---
+++ Практика-1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            double num1, num2, result;
+            do
+            {
+                Console.WriteLine("Выберите операцию:");
+                Console.WriteLine("1. Сложение");
+                Console.WriteLine("2. Вычитание");
+                Console.WriteLine("3. Умножение");
+                Console.WriteLine("4. Деление");
+                Console.WriteLine("5. Возведение в степень");
+                Console.WriteLine("6. Квадратный корень");
+                Console.WriteLine("7. 1% от числа");
+                Console.WriteLine("8. Факториал");
+                Console.WriteLine("9. Выход");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Введите первое число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        Console.Write("Введите второе число: ");
+                        num2 = double.Parse(Console.ReadLine());
+                        result = num1 + num2;
+                        Console.WriteLine($"Результат: {result}");
+                        break;
+                    case 2:
+                        Console.Write("Введите первое число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        Console.Write("Введите второе число: ");
+                        num2 = double.Parse(Console.ReadLine());
+                        result = num1 - num2;
+                        Console.WriteLine($"Результат: {result}");
+                        break;
+                    case 3:
+                        Console.Write("Введите первое число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        Console.Write("Введите второе число: ");
+                        num2 = double.Parse(Console.ReadLine());
+                        result = num1 * num2;
+                        Console.WriteLine($"Результат: {result}");
+                        break;
+                    case 4:
+                        Console.Write("Введите первое число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        Console.Write("Введите второе число: ");
+                        num2 = double.Parse(Console.ReadLine());
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Ошибка: деление на ноль");
+                        }
+                        else
+                        {
+                            result = num1 / num2;
+                            Console.WriteLine($"Результат: {result}");
+                        }
+                        break;
+                    case 5:
+                        Console.Write("Введите число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        Console.Write("Введите степень: ");
+                        int power = int.Parse(Console.ReadLine());
+                        result = Math.Pow(num1, power);
+                        Console.WriteLine($"Результат: {result}");
+                        break;
+                    case 6:
+                        Console.Write("Введите число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        if (num1 < 0)
+                        {
+                            Console.WriteLine("Ошибка: квадратный корень из отрицательного числа");
+                        }
+                        else
+                        {
+                            result = Math.Sqrt(num1);
+                            Console.WriteLine($"Результат: {result}");
+                        }
+                        break;
+                    case 7:
+                        Console.Write("Введите число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        result = num1 / 100;
+                        Console.WriteLine($"Результат: {result}");
+                        break;
+                    case 8:
+                        Console.Write("Введите число: ");
+                        num1 = double.Parse(Console.ReadLine());
+                        result = 1;
+                        for (int i = 2; i <= num1; i++)
+                        {
+                            result *= i;
+                        }
+                        Console.WriteLine($"Результат: {result}");
+                        break;
+                    case 9:
+                        Console.WriteLine("Выход");
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка: неверный выбор операции");
+                        break;
+                }
+            } while (choice != 9);
+        }
+    }
+}
---
+++ Практика-2.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+        while (true)
+        {
+            Console.WriteLine("Выберите программу для запуска:");
+            Console.WriteLine("1. Программа 1");
+            Console.WriteLine("2. Программа 2");
+            Console.WriteLine("3. Программа 3");
+            Console.WriteLine("4. Выйти");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                RunProgram1();
+            }
+            else if (choice == 2)
+            {
+                RunProgram2();
+            }
+            else if (choice == 3)
+            {
+                RunProgram3();
+            }
+            else if(choice == 4)
+            {
+                break;
+            }
+            {
+                Console.WriteLine("Такой команды нет"); 
+                
+            }
+
+
+
+        }
+
+    
+    }
+
+    private static void RunProgram1()
+    {
+        Random random = new Random();
+        int random_number = random.Next(0, 101);
+        Console.WriteLine("Угадай число от 0 до 100 (включительно)");
+        int user_number = Convert.ToInt32(Console.ReadLine());
+
+        while(true)
+        {
+            if (user_number>random_number)
+            {
+                Console.WriteLine("Надо меньше");
+            }
+            else if (user_number<random_number)
+            {
+                Console.WriteLine("Надо больше");
+            }
+            else
+            {
+                Console.WriteLine("Ебать попадание");
+                break;
+            }
+            user_number = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    private static void RunProgram2()
+    {
+        int[,] table = new int[9,9];
+        for(int number_row = 1;number_row<=9;number_row++) { 
+            for(int number_col = 1; number_col<=9; number_col++)
+            {
+                table[number_row-1, number_col-1]= number_row*number_col;
+            }
+        }
+        for(int row = 0; row<9; row++)
+        {
+            for (int col = 0; col<9; col++)
+            {
+                Console.Write($"{table[row,col]}\t");
+            }
+            Console.WriteLine();
+        }
+        
+        
+    }
+
+    private static void RunProgram3()
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        for(int counter = 1; counter<=number; counter++)
+        {
+            if (number%counter==0)
+            {
+                Console.Write($"{counter}  ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
